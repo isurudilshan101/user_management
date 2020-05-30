b2e5b47b2d4574f7b21d3bd883c3ba5d50265099
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-05-31 03:34:43 +0530

    comment 2

--- ums/inc/connection.php
+++ ums/inc/connection.php
@@ -3,15 +3,13 @@
 	$dbhost = 'localhost';
 	$dbuser = 'root';
 	$dbpass = '';
-	$dbname = 'userdb'; 
+	$dbname = 'userdb1'; 
 
 	$connection = mysqli_connect('localhost', 'root', '', 'userdb');
 
 	// Checking the connection
 	if (mysqli_connect_errno()) {
 		die('Database connection failed ' . mysqli_connect_error());
-	} else {
-		echo "success";
-	}
+	} 
 
 ?>
--- ums/index.php
+++ ums/index.php
@@ -1,12 +1,31 @@
 <?php require_once('inc/connection.php'); ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-	<meta charset="UTF-8">
 	<title>Log In - User Management System</title>
 </head>
 <body>
-	
+
+	<div class="login">
+		
+		<form action="index.php" method="post"></form>
+		<fieldset>
+			<legend><h1>Log In</h1></legend>
+			<p>
+				<label for="">Username:</label>
+				<input type="text" name="email" id="" placeholder="Email Address">
+
+			</p>
+
+			<p>
+				<label for="">Password</label>
+				<input type="Password" name="" id="" placeholder="Password">
+			</p>
+		</fieldset>
+
+	</div> <!--login-->
+
+
 </body>
 </html>
 <?php mysqli_close($connection); ?>
