b5f3d84657d56782ae28b14d51b11659cadc4b27
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-06-04 02:54:12 +0530

    comment 5

--- ums/index.php
+++ ums/index.php
@@ -1,4 +1,41 @@
 <?php require_once('inc/connection.php'); ?>
+<?php 
+
+// check for form submission
+if(isset($_POST['submit'])){
+
+$errors=array();
+// check if the username and password has been entered
+	if(!isset($_POST['email']) || strlen(trim($_POST['email']))<1){
+		$errors[]="User name is missing/Invalid;";
+	}
+
+	if(!isset($_POST['password']) || strlen(trim($_POST['password']))<1){
+		$errors[]='Password is missing/Invalid';
+	}
+//check if there are any errors in the form
+if(empty($errors)){
+//save username and passwword into variables
+	$email=mysqli_real_escape_string($connection,$_POST['email']);
+
+	$password=mysqli_real_escape_string($connection,$_POST['password']);
+	$hashed_password=sha1($password);
+
+
+//prepare database query
+
+//check if the user is valid
+
+//redirect to users.php
+
+//if not, display the error 
+}	
+
+
+
+} 
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -24,7 +61,7 @@
 
 			<p>
 				<label for="">Password</label>
-				<input type="Password" name="" id="" placeholder="Password">
+				<input type="password" name="password" id="" placeholder="Password">
 			</p>
 
 			<button type="submit" name="submit">Log In</button>
