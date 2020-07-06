d727ed0e963ed0fd774e067e3f25629d056e286f
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-07-06 18:50:49 +0530

    comment 16

--- ums/users.php
+++ ums/users.php
@@ -19,6 +19,10 @@
 			$user_list .="<td>{$user['first_name']}</td>";
 			$user_list .="<td>{$user['last_name']}</td>";
 			$user_list .="<td>{$user['last_login']}</td>";
+			$user_list .="<td><a href=\"modify-user.php?user_id={$user['id']}\">Edit</a></td>";
+			$user_list .="<td><a href=\"modify-user.php?user_id={$user['id']}\">Delete</a></td>";
+
+
 
 			$user_list .="</tr>";
 		}
