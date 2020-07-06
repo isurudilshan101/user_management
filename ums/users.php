9922ea28e79148af8b7bc6da67d2b60dc2437cb5
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-07-06 18:28:14 +0530

    comment 15

--- ums/users.php
+++ ums/users.php
@@ -6,6 +6,26 @@
 	if (!isset($_SESSION['user_id'])){
 		header('Location:index.php');
 	}
+
+	$user_list='';
+
+	//getting the list of users
+	$query="SELECT * FROM users WHERE is_deleted=0 ORDER BY first_name";
+	$users=mysqli_query($connection,$query);
+
+	if($users){
+		while($user=mysqli_fetch_assoc($users)){
+			$user_list .="<tr>";
+			$user_list .="<td>{$user['first_name']}</td>";
+			$user_list .="<td>{$user['last_name']}</td>";
+			$user_list .="<td>{$user['last_login']}</td>";
+
+			$user_list .="</tr>";
+		}
+	}else{
+		echo "Database query failed";
+	}
+
 ?>
 
 <!DOCTYPE html>
@@ -29,6 +49,8 @@
 			<th>Last Login</th>
 			<th>Edit</th>
 			<th>Delete</th>
+
+			<?php echo $user_list; ?>
 
 		</tr>
 
