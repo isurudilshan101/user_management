9f7a172a4921648ab8d3a9a0d1730054b14602da
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-07-07 10:34:18 +0530

    comment 27

--- ums/add-user.php
+++ ums/add-user.php
@@ -1,6 +1,18 @@
 <?php session_start(); ?>
 <?php require_once('inc/connection.php'); ?>
-<?php require_once('inc/functions.php'); ?>
+<?php require_once('inc/functions.php'); ?> 
+<?php 
+
+	$errors=array();
+
+	if(isset($_POST['submit'])){
+		//checking required fields
+		if(empty($_POST['first_name'])){
+			$errors[]='First Nae is required';
+		}
+	}
+
+ ?>
 
 
 
@@ -18,26 +30,35 @@
 
 	<main>
 		<h1>Add New User<span><a href="users.php"> < Back to User List</a></span>	</h1>	
+
+		<?php 
+
+		if(!empty($errors)){
+			echo 'There ware errors on your form.';
+		}
+
+
+		 ?>
 		
 		<form action="add-user.php" method="post" class="userform">
 			<p>
-				<label for="">Fiest Name:</label>
-				<input type="text" name="first_name" required>
+				<label for="">First Name:</label>
+				<input type="text" name="first_name" >
 			</p>
 
 			<p>
 				<label for="">Last Name:</label>
-				<input type="text" name="last_name" required>
+				<input type="text" name="last_name" >
 			</p>
 
 			<p>
 				<label for="">Email Address:</label>
-				<input type="Email" name="email" required>
+				<input type="Email" name="email" >
 			</p>
 
 			<p>
 				<label for="">New Password:</label>
-				<input type="password" name="password" required>
+				<input type="password" name="password" >
 			</p>
 
 			<p>
