4c1e3914f52d79c7ea16630a998a8dc846335f3e
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-07-07 14:06:01 +0530

    comment 33

--- ums/add-user.php
+++ ums/add-user.php
@@ -9,22 +9,24 @@
 
 			//checking required fields
 
-			$max_len_fields=array('first_name' =>50,'last_name' =>100,'email' =>100,'password' =>40);
-
-			foreach ($max_len_fields as $field =>$max_len) {
-				if(strlen(trim($_POST[$field]))<$max_len){
-			$errors[]= $field . ' must be less than ' . $max_len . 'characters';
-		}
-
-	}
-
-	//checking max length
-
-		$req_fields=array('first_name','last_name','email','password');
+				$req_fields=array('first_name','last_name','email','password');
 
 			foreach ($req_fields as $field) {
 				if(empty(trim($_POST[$field]))){
 			$errors[]= $field . ' is required';
+		}
+
+	}
+
+	
+
+	//checking max length
+
+			$max_len_fields=array('first_name' =>50,'last_name' =>100,'email' =>100,'password' =>40);
+
+			foreach ($max_len_fields as $field =>$max_len) {
+				if(strlen(trim($_POST[$field]))>$max_len){
+			$errors[]= $field . ' must be less than ' . $max_len . 'characters';
 		}
 
 	}
