787204355e297aea7f6b8d6c36e2366a658942ff
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-07-07 09:04:24 +0530

    comment 26

--- ums/add-user.php
+++ ums/add-user.php
@@ -19,6 +19,33 @@
 	<main>
 		<h1>Add New User<span><a href="users.php"> < Back to User List</a></span>	</h1>	
 		
+		<form action="add-user.php" method="post" class="userform">
+			<p>
+				<label for="">Fiest Name:</label>
+				<input type="text" name="first_name">
+			</p>
+
+			<p>
+				<label for="">Last Name:</label>
+				<input type="text" name="last_name">
+			</p>
+
+			<p>
+				<label for="">Email Address:</label>
+				<input type="Email" name="email">
+			</p>
+
+			<p>
+				<label for="">New Password</label>
+				<input type="password" name="password">
+			</p>
+
+			<p>
+				<label for="">&nbsp;</label>
+			</p>
+			
+		</form>
+
 
 	</main>
 </body>
